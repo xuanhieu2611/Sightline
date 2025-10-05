1ab0fb6d9115f1a036a1e1f3656e6a4f8c8459e6
xuanhieu2611 <hieulexuan261103@gmail.com>
2025-10-05 09:33:42 -0700

    Disable ESLint during builds for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -122,6 +122,9 @@ const withPWA = require("next-pwa")({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["localhost"],
   },
